4128a807a1741c3fa5af2d574aeef4a65714867a
Andrew Anthony <a.anthony1835@gmail.com>
2021-01-09 14:33:21 -0800

    Broke down assignment into manageable TODOS

---
+++ Develop/index.js
@@ -0,0 +1,30 @@
+// TODO: Include packages needed for this application
+
+// TODO: Create an array of questions for user input
+const questions = [];
+
+// TODO: Create a function to write README file
+function writeToFile(fileName, data) {}
+
+// TODO: Create a function to initialize app
+function init() {}
+
+// Function call to initialize app
+init();
+
+
+
+
+
+
+
+
+// TODO: Install all the packages
+// TODO: Declare the packages at the top of the page
+// TODO: Prompt the user in node with all the questions that are outlined in the description for this assignment
+// TODO: Create a README template 
+// TODO: Copy that template to a variable using backticks 
+// TODO: Generate a README file
+// TODO: Push the information from the variable onto the generated README file
+// TODO: Do a Screencastify video 
+// TODO: Submit everything, making sure that you've met all the requirements for the generator and for the sumbission guidelines 
